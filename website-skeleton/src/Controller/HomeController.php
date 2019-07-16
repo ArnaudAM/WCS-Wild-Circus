a47826ebd6a786bf5e65b5c9ee223a19c6c0c396
Arnaud Auzou <arnaud.auzoudm@gmail.com>
2019-07-16 16:05:09 +0200

    Ajout de webpack encore

--- website-skeleton/src/Controller/HomeController.php
+++ website-skeleton/src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 
 namespace App\Controller;
 
+use phpDocumentor\Reflection\Types\String_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
